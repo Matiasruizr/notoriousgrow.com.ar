32460fab47a0d01ef6fb5ab1de6cf7e157f96e93
totomas <josetob14@gmail.com>
2019-07-12 13:32:08 -0400

    Landing notoriousgrow

---
+++ layouts/default.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="bg">
+    <nav
+      class="navbar header has-shadow is-dark"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="navbar-brand">
+        <a
+          class="navbar-item"
+          href="/"
+        >
+          <img
+            src="~/assets/logo-notorious.png"
+            alt="Buefy"
+            height="28"
+          >
+        </a>
+
+        
+      </div>
+    </nav>
+
+    <section class="main-content columns">
+      
+
+      <div class="container column is-10">
+        <nuxt />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          title: 'Home',
+          icon: 'home',
+          to: { name: 'index' }
+        },
+        {
+          title: 'Inspire',
+          icon: 'lightbulb',
+          to: { name: 'inspire' }
+        }
+      ]
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.bg{
+  background: url(~assets/cannabis.jpg);
+  background-size: cover;
+  background-attachment: fixed;
+  @media screen and(max-width:769px){
+     float: left;
+  }
+}
+.columns{
+  margin-right: 0 !important;
+  &:last-child{
+    margin-bottom: 0;
+  }
+}
+.main-content{
+  background: rgba(27, 26, 27, 0.7);
+  @media screen and(max-width:769px){
+    float: left;
+  }
+}
+.navbar-brand{
+  margin-left: 50px;
+  @media screen and(max-width:769px){
+    margin-left: 20px;
+  }
+  img{
+    width: 50px;
+    max-height: none;
+  }
+}
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,86 @@
+<template>
+  <section class="section">
+    <div class=" logo column is-12">
+      <img src="~/assets/logo-notorious.png">
+    </div>
+      <div class="btn column is-6">
+        <a href="https://notorious.cl/"><h1>Notorious Chile</h1>
+        <img src="~/assets/chile.png"></a>
+      </div>
+      <div class="btn column is-6">
+        <a href="http://notoriousgrow.com.ar/"><h1>Notorious Argentina</h1>
+        <img src="~/assets/argentina.png"></a>
+      </div>
+  </section>
+</template>
+
+<script>
+import Card from '~/components/Card'
+
+export default {
+  name: 'HomePage',
+
+  components: {
+    Card
+  }
+}
+</script>
+<style lang="scss" scoped>
+.logo{
+  width: 100%;
+  float: left;
+  img{
+    width: 15%;
+    display: block;
+    margin: 0 auto;
+    @media screen and(max-width:426px){
+      width: 30%;
+    }
+  }
+}
+.btn{
+  
+  margin: 60px 0 110px 0;
+  float: left;
+  @media screen and(max-width:769px){
+    width: 50%;
+    margin-bottom: 130px;
+  }
+  @media screen and(max-width:426px){
+    width: 100%;
+    margin: 18px 0;
+  }
+  @media screen and(max-width:376px){
+    margin: 28px 0;
+  }
+  @media screen and(max-width:321px){
+    margin: 30px 0;
+  }
+  h1{
+    font-weight: bold;
+    color: #ffffff;
+    float: left;
+    margin-bottom: 30px;
+    font-size: 32px;
+    width: 100%;
+    text-align: center;
+    transition: 200ms;
+    &:hover{
+      color: #7e005a;
+    }
+    @media screen and(max-width:426px){
+      font-size:25px;
+    }
+  }
+  img{
+    display: block;
+    margin: 0 auto;
+    width: 40%;
+    transition: 200ms;
+    &:hover{
+      transform: scale(1.1);
+    }
+  }
+}
+</style>
+
