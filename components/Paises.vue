924347fe99a125e02ffb692ff3dfa41f152eaaf9
Matias Ruiz <matias@acomeradmin.cl>
2020-03-06 11:24:24 -0300

    Links funcionales

--- components/Paises.vue
+++ components/Paises.vue
@@ -2,12 +2,16 @@
       <section class="container">
             <div class="banderas">
                   <span class="has-text-centered">
+                  <a href="https://notorious.cl/" target="_blank">
                         <img src="../assets/img/chile.png" alt="chile">
                         <h1>chile</h1>
+                  </a>
                   </span>
                   <span class="has-text-centered"> 
+                        <a href="https://notoriousgrow.com.ar/" target="_blank">
                         <img src="../assets/img/argentina.png" alt="argentina">
                         <h1>argentina</h1>
+                        </a>
                   </span>
                   
             </div>
@@ -17,31 +21,41 @@
                   <p>Haz click en cualquiera de los iconos para comunicarte con nosotros</p>
             </div>
             <div class="iconos has-text-centered">
-                  <b-icon
-                        class="icono"
-                        icon="whatsapp"
-                        size="is-small">
-                  </b-icon>
-                  <b-icon
-                        class="icono"
-                        icon="instagram"
-                        size="is-small">
-                  </b-icon>
-                  <b-icon
-                        class="icono"
-                        icon="facebook"
-                        size="is-small">
-                  </b-icon>
-                  <b-icon
-                        class="icono"
-                        icon="email"
-                        size="is-small">
-                  </b-icon>
-                  <b-icon
-                        class="icono"
-                        icon="phone"
-                        size="is-small">
-                  </b-icon>
+                  <a href="https://api.whatsapp.com/send?phone=56225940420&text=Hola,%20te%20escribo%20desde%20Notoriousgrow.com" target="_blank">
+                        <b-icon
+                              class="icono"
+                              icon="whatsapp"
+                              size="is-small">
+                        </b-icon>
+                  </a>
+                  <a href="https://www.instagram.com/notorious_grow/" target="_blank">
+                        <b-icon
+                              class="icono"
+                              icon="instagram"
+                              size="is-small">
+                        </b-icon>
+                  </a>
+                  <a href="https://www.facebook.com/Notorious.cl/" target="_blank">
+                        <b-icon
+                              class="icono"
+                              icon="facebook"
+                              size="is-small">
+                        </b-icon>
+                  </a>
+                  <a href="mailto:contacto@notorious.cl">
+                        <b-icon
+                              class="icono"
+                              icon="email"
+                              size="is-small">
+                        </b-icon>
+                  </a>
+                  <a href="tel:+56225940420">
+                        <b-icon
+                              class="icono"
+                              icon="phone"
+                              size="is-small">
+                        </b-icon>
+                  </a>
             </div>
       </section>
 </template>
